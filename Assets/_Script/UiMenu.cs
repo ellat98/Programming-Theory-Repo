b220e2615ac00727d4a1728739fdf08723788fcd
ellat98 <159927671+ellat98@users.noreply.github.com>
2024-03-01 17:22:50 +0100

    Create car script

--- Assets/_Script/UiMenu.cs
+++ Assets/_Script/UiMenu.cs
@@ -7,7 +7,7 @@ using TMPro;
 
 public class UiMenu : MonoBehaviour
 {
-    public TMP_InputField input;
+    [SerializeField] TMP_InputField input;
     // Start is called before the first frame update
     void Start()
     {
